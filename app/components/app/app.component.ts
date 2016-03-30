056523933c7786a49b3c2e9444c489254922d4ec
Tony Sullivan <tony.sullivan@fanbeat.com>
2016-03-30 15:05:31 -0500

    Removing relative path from app.component HTML & style urls

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent { }
